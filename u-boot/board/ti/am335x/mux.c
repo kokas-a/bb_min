cadf0d0a123672333fe63e150712580ca24c4cb2
Nikolay Agishev <agishev@synopsys.com>
2023-04-15 23:07:55 +0300

    Fix Nand pinmux

--- u-boot/board/ti/am335x/mux.c
+++ u-boot/board/ti/am335x/mux.c
@@ -420,7 +420,7 @@ void enable_board_pin_mux(void)
 			configure_module_pin_mux(mii1_pin_mux);
 		}
 		configure_module_pin_mux(mmc0_pin_mux);
-#if defined(CONFIG_NAND) && defined(CONFIG_EMMC_BOOT)
+#if defined(CONFIG_NAND) && defined(CONFIG_SD_BOOT)
 		configure_module_pin_mux(nand_pin_mux);
 #elif defined(CONFIG_NOR) && defined(CONFIG_EMMC_BOOT)
 		configure_module_pin_mux(bone_norcape_pin_mux);
